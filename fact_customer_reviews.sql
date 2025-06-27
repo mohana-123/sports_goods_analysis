d4352e4fd91351e43254cdc80110a9ac15fe3190
Mohana Sruthy Mada <104384297+mohana-123@users.noreply.github.com>
2025-06-27 12:00:49 +0530

    Add files via upload

---
+++ fact_customer_reviews.sql
@@ -0,0 +1,17 @@
+-- select
+-- 	*
+-- from dbo.customer_reviews
+
+-- ***************************************************************************************
+-- ***************************************************************************************
+
+-- Query to clear the white spaces in ReviewText
+-- check them if present
+select
+	ReviewID,
+	CustomerID,
+	ProductID,
+	ReviewDate,
+	Rating,
+	REPLACE(ReviewText,'  ', ' ') as ReviewText
+from dbo.customer_reviews;
